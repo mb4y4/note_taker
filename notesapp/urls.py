57ed6934d2b7e359171dcdcdbc24d6cd579388fb
Mb4y4 <d.sommy@gmail.com>
2024-01-12 17:35:28 +0300

    notes viewing

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NotesListView.as_view()),
-    path('notes/<int:pk>/', views.NotesDetailView.as_view()),
+    path('notes', views.NotesListView.as_view(), name='notes.list'),
+    path('notes/<int:pk>/', views.NotesDetailView.as_view(), name='notes.description'),
     # path('add/', views.add),
     # path('edit/<int:id>/', views.edit),
     # path('delete/<int:id>/', views.delete),

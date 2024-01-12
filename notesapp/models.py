1b339b05f8536f0bd2b2d72ca8506fe29837e7a1
Mb4y4 <d.sommy@gmail.com>
2024-01-13 01:01:53 +0300

    create login

--- notesapp/models.py
+++ notesapp/models.py
@@ -1,10 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Notes(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    
-    # def __str__(self):
-    #     return self.title
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
--- notesapp/views.py
+++ notesapp/views.py
@@ -1,38 +1,63 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from .models import Notes
+
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .forms import NotesForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
-class NotesDeleteView(DeleteView):
+class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     template_name = 'notesapp/notes_confirm_delete.html'
     success_url = '/smart/notes'
+    login_url ='/admin'
     
-class NotesUpdateView(UpdateView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+    
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     #template_name = 'notesapp/notes_form.html'
     #fields = ['title', 'description']
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url ='/admin'
     
-class NotesCreateView(CreateView):
+    def get_queryset(self):
+        return self.request.user.notes.all()
+    
+class NotesCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     template_name = 'notesapp/notes_form.html'
     #fields = ['title', 'description']
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url ='/admin'
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
     
     
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     template_name = 'notesapp/notes_list.html'
     context_object_name = 'notes'
+    login_url ='/admin'
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 
 
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Notes
     template_name = 'notesapp/notes_detail.html'
     context_object_name = 'note'
+    login_url ='/admin'
+    
+    def get_queryset(self):
+        return self.request.user.notes.all()
 

e7de08682260507191383b654a1f7bea00a3aca0
Mb4y4 <d.sommy@gmail.com>
2024-01-12 12:39:15 +0300

    first commit(setting up environment)

---
+++ homeapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def home(request):
+    return render(request, 'homeapp/welcome.html', {'today': datetime.today})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'homeapp/authorized.html', {'today': datetime.today})

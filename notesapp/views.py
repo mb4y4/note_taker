732a0c747d9c13f5e31edb068c00c64ce2e63994
Mb4y4 <d.sommy@gmail.com>
2024-01-13 00:47:09 +0300

    'Delete' note

--- notesapp/views.py
+++ notesapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Notes
-from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .forms import NotesForm
 
 # Create your views here.
+class NotesDeleteView(DeleteView):
+    model = Notes
+    template_name = 'notesapp/notes_confirm_delete.html'
+    success_url = '/smart/notes'
+    
 class NotesUpdateView(UpdateView):
     model = Notes
     #template_name = 'notesapp/notes_form.html'

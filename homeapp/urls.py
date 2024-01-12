8052710217c7ab656d86ae1c2113789de49e7ac3
Mb4y4 <d.sommy@gmail.com>
2024-01-13 02:06:58 +0300

    signup note

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('login/', views.LoginInterfaceView.as_view(), name='login'),
     path('logout/', views.LogoutInterfaceView.as_view(), name='logout'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from datetime import datetime
-# from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+
+from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+
+from django.shortcuts import redirect
+
 # Create your views here.
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'homeapp/signup.html'
+    success_url = '/smart/notes/'
+    
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+        return super().get(request, *args, **kwargs)
 
 class LoginInterfaceView(LoginView):
     template_name = 'homeapp/login.html'
